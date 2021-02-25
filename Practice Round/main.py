4a9e3f70fe6fa412f5e277f36e9bcbaf7fe5fe64
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-02-25 14:46:34 +0100

    Pimp main function.

--- Practice Round/main.py
+++ Practice Round/main.py
@@ -1,27 +1,54 @@
+import logging
+import importlib.util
+import pkgutil
+from pathlib import Path
+
+
+def get_available_solver(dir_name, baseclass):
+    spec = importlib.util.find_spec(Path(dir_name).name)
+    for (_, name, _) in pkgutil.iter_modules([dir_name]):
+        try:
+            importlib.import_module(f'.{name}', spec.name)
+        except ModuleNotFoundError:
+            pass
+    _classes = {str(cls.__name__).lower(): cls for cls in baseclass.__subclasses__()}
+    return [str(_cls).lower() for _cls in _classes], _classes
+
+
 if __name__ == '__main__':
     import importlib
     import argparse
+
+    cls_names, classes = get_available_solver("solver", importlib.import_module("solver.basesolver").BaseSolver)
 
     parser = argparse.ArgumentParser()
 
     # need to be
     parser.add_argument("input", help="input file")
-
     parser.add_argument("--output", help="output file")
-    parser.add_argument("--solver", type=str, default="example")
+
+    parser.add_argument('--debug', action='store_true', help='set debug level')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--solver", help="available solver", type=str, choices=cls_names, default="example")
+    group.add_argument('--score', help="computes score and insights of given data set", action='store_true')
+
     args = parser.parse_args()
 
-    solver = None
-    # try load the given solver
-    try:
-        solver = importlib.import_module('.'.join(["solver", args.solver]))
-    except ImportError as e:
-        parser.print_help()
-        print(e)
-        exit(1)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    LOGGER = logging.getLogger(__name__)
 
+    if args.score:
+        scoring = importlib.import_module("solver.scoring")
+        score = scoring.compute_score(args.input, args.output)
+        exit(0)
+
+    # get the chosen solver
+    Solver = classes[args.solver]
     # solver init with filepath
-    solver = solver.Solver(args.input)
+    solver = Solver(args.input)
 
     # solve the problem with given input
     success = solver.solve()
